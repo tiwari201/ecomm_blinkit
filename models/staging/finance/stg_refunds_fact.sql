aa659fe4a2f96b9bfc9cf47bc8ba8f8893980608
Abhishek Tiwari <abhit4343@Abhisheks-Air.lan>
2025-09-08 03:06:36 +0530

    Test comments

--- models/staging/finance/stg_refunds_fact.sql
+++ models/staging/finance/stg_refunds_fact.sql
@@ -1,5 +1,5 @@
 SELECT  refund_id                   as refund_id, -- Primary key, unique and not null
-        order_id                    as order_id,
+        order_id                    as order_id, -- Foreign key
         amount_refunded             as amount_refunded,
         refund_reason               as refund_reason
 FROM {{ source('db_src_finance', 'refunds_fact') }}
