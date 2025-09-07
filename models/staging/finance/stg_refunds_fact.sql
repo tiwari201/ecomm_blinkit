c3e626bc55d50bd8bfd1113db6570943904b1639
Abhishek Tiwari <abhit4343@Abhisheks-Air.lan>
2025-09-08 03:13:22 +0530

    Added current timestamp

--- models/staging/finance/stg_refunds_fact.sql
+++ models/staging/finance/stg_refunds_fact.sql
@@ -1,5 +1,6 @@
 SELECT  refund_id                   as refund_id, -- Primary key, unique and not null
         order_id                    as order_id, -- Foreign key
         amount_refunded             as amount_refunded,
-        refund_reason               as refund_reason
+        refund_reason               as refund_reason,
+        current_timestamp()          as dbt_updated_at
 FROM {{ source('db_src_finance', 'refunds_fact') }}
