dc070f9d039ea8f765f5dc88d63bd876c343aef6
Abhishek Tiwari <abhit4343@Abhisheks-Air.lan>
2025-09-08 04:20:04 +0530

    Added current timestamp

--- models/staging/finance/stg_commission_fact.sql
+++ models/staging/finance/stg_commission_fact.sql
@@ -2,5 +2,6 @@ SELECT  order_id                    as order_id,  -- Primary Key added for dbt
         delivery_partner_id         as delivery_partner_id,
         commission_amount           as commission_amount,
         payout_status               as delivery_partner_payout_status,
-        current_timestamp()          as dbt_updated_at
+        current_timestamp()          as dbt_updated_at,
+        current_timestamp() as dbt_run_at
 FROM {{ source('db_src_finance', 'commission_fact') }}
