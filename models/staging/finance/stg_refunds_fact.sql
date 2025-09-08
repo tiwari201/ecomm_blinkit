d7eadaccc12b485821ab80aa4e1081721ce9d5c0
Abhishek Tiwari <abhit4343@Abhisheks-Air.lan>
2025-09-08 10:56:42 +0530

    Removed timestamp fom refunds

--- models/staging/finance/stg_refunds_fact.sql
+++ models/staging/finance/stg_refunds_fact.sql
@@ -2,6 +2,5 @@ SELECT  refund_id                   as refund_id, -- Primary key, unique and not
         order_id                    as order_id, -- Foreign key
         amount_refunded             as amount_refunded,
         refund_reason               as refund_reason,
-        current_timestamp()          as dbt_updated_at,
-        current_timestamp() as dbt_run_at
+        current_timestamp()          as dbt_updated_at
 FROM {{ source('db_src_finance', 'refunds_fact') }}
