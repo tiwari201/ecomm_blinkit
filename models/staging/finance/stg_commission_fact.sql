2e4ccca7b90dbf78b24de7edde3629c0b29cb6ef
Abhishek Tiwari <abhit4343@Abhisheks-Air.lan>
2025-09-08 12:54:44 +0530

    Rmv Placeholder col

--- models/staging/finance/stg_commission_fact.sql
+++ models/staging/finance/stg_commission_fact.sql
@@ -1,7 +1,5 @@
 SELECT  order_id                    as order_id,  -- Primary Key added for dbt
         delivery_partner_id         as delivery_partner_id,
         commission_amount           as commission_amount,
-        payout_status               as delivery_partner_payout_status,
-        current_timestamp()          as dbt_updated_at,
-        null as placeholdeer_column -- Placeholder column to ensure at least one transformation
+        payout_status               as delivery_partner_payout_status
 FROM {{ source('db_src_finance', 'commission_fact') }}
